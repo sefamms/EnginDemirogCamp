ac2d680a7d9a972984776956cf34021dd5fac5a9
sefamms <smemis412@gmail.com>
2021-02-05 10:06:10 +0300

    commit

--- MyDictionary/Program.cs
+++ MyDictionary/Program.cs
@@ -9,53 +9,38 @@ namespace MyDictionary
             Console.WriteLine("Hello World!");
         }
     }
+
     class MyDictionary<K, V>
     {
-        K[] key;  //boş dizi oluştur
-        K[] tempKey; //geçiçi boş bir dizi oluştur
 
-        V[] value;
-        V[] tempValue;
-
+        K[] keys;
+        V[] values;
         public MyDictionary()
         {
-            key = new K[0];  //Program başladığından new le ve içerisinde 0 eleman olsun
-            value = new V[0];
+            keys = new K[0];
+            values = new V[0];
         }
 
-
-        public void Add(K keyItem, V valueItem)
+        public void Add(K key, V value)
         {
-            tempKey = key;
 
-            key = new K[key.Length + 1];
 
-            for (int i = 0; i < tempKey.Length; i++)
+            K[] tempArrayKey = keys;
+
+            keys = new K[keys.Length + 1];
+            V[] tempArrayValue = values;
+            values = new V[values.Length + 1];
+            for (int i = 0; i < tempArrayKey.Length; i++)
             {
-                key[i] = tempKey[i];
+                keys[i] = tempArrayKey[i];
             }
-
-            key[key.Length - 1] = keyItem;
-
-
-
-            tempValue = value;
-
-            value = new V[value.Length + 1];
-
-            for (int i = 0; i < tempValue.Length; i++)
+            keys[keys.Length - 1] = key;
+            for (int i = 0; i < tempArrayValue.Length; i++)
             {
-                value[i] = tempValue[i];
+                values[i] = tempArrayValue[i];
             }
-
-            value[value.Length - 1] = valueItem;
-
+            values[values.Length - 1] = value;
 
         }
-
-        public K[] Key { get { return key; } }
-
-        public V[] Value { get { return value; } }
-
     }
 }
